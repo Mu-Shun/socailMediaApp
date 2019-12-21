cc2a6f1ba6bce556711f83ddc4304e5266c94ca2
Mu-Shun Lee <mushunle@usc.edu>
2019-12-20 23:47:57 -0800

    changes made

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 // load modules
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const passport = require('passport')
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 // Connect to MongoURI expoerted from external file
 const keys = require('./config/keys');
+// User Collection
+const User = require('./models/user')
+require('./passport/google.passport');
 // initialize application
 const app = express();
+//Exoress config
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'keyboard cat', 
+                resave: true,
+                saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+//set global vars for user
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+})
 // setup template engine
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -15,6 +36,7 @@ app.set('view engine', 'handlebars');
 // setup static file to serve css, javascript and images
 app.use(express.static('public'));
 // connect to remote database
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.MongoURI, {
     useNewUrlParser: true 
 })
@@ -33,7 +55,29 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) =>{
     res.render('about');
 });
+// Goole Auth Route
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/profile');
+  });
+app.get('/profile', (req, res) => {
+    User.findById({_id:req.user._id})
+    .then((user) => {
+        res.render('profile', {
+            user:user
+        });
+    })
+});
+// handle user logout
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+})
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
