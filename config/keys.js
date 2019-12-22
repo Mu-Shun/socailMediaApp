efc9857fd6399c8db2141c828aa5340a0e09891a
Mu-Shun Lee <mushunle@usc.edu>
2019-12-21 16:53:50 -0800

    config keys 2

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    MongoURI: 'mongodb+srv://socialmediaapp:a101701007@onlinedatingapp-j72wp.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '872289267714-f5ej6aqc4q43b090dsjr2go7uj1k4e58.apps.googleusercontent.com',
-    GoogleClientSecret: '9MC8ondjXzq-k-_R3NTbohuv'
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./key-prod')
+}else {
+    module.exports = require('./keys-dev')
 }
