59bc0388d9dde22826c165b5429d499a9f0a8aad
Mu-Shun Lee <mushunle@usc.edu>
2019-12-21 16:39:09 -0800

    config keys

--- config/key-prod.js
+++ config/key-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://socialmediaapp:a101701007@onlinedatingapp-j72wp.mongodb.net/test?retryWrites=true&w=majority',
-    GoogleClientID: '900665742527-5unvs9rcmo950t7a5fa70m9bbvku9r36.apps.googleusercontent.com',
-    GoogleClientSecret: 'xAyX4gyww2r8kEJ9JBh8FfFp'
+    MongoURI: process.env.MONGO_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
